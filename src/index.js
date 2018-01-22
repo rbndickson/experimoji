758e5f4add405b6dd08a5ae50ecece159eb13c8f
Robin Dickson <robindickson@gmail.com>
2018-01-22 17:40:09 +0700

    feat: Add redux store

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import reducer from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
