f0aa649e69826cee10bc8a2f89fa4aeba0e0cf0d
Robin Dickson <robindickson@gmail.com>
2018-01-22 22:19:01 +0700

    feat: Add animals flashcard data

---
+++ src/flashcardData.js
@@ -0,0 +1,282 @@
+export const initialFlashcardsState = {
+  ant: {
+    emojiCode: "1f41c",
+    english: "ant",
+    mexicanSpanish: "la hormiga"
+  },
+  bat: {
+    emojiCode: "1f987",
+    english: "bat",
+    mexicanSpanish: "el murciélago"
+  },
+  bear: {
+    emojiCode: "1f43b",
+    english: "bear",
+    mexicanSpanish: "el oso"
+  },
+  bee: {
+    emojiCode: "1f41d",
+    english: "bee",
+    mexicanSpanish: "la abeja"
+  },
+  bird: {
+    emojiCode: "1f426",
+    english: "bird",
+    mexicanSpanish: "el ave"
+  },
+  boar: {
+    emojiCode: "1f417",
+    english: "boar",
+    mexicanSpanish: "el jabalí"
+  },
+  butterfly: {
+    emojiCode: "1f98b",
+    english: "butterfly",
+    mexicanSpanish: "la mariposa"
+  },
+  camel: {
+    emojiCode: "1f42b",
+    english: "camel",
+    mexicanSpanish: "el camello"
+  },
+  cat: {
+    emojiCode: "1f431",
+    english: "cat",
+    mexicanSpanish: "el gato"
+  },
+  caterpiller: {
+    emojiCode: "1f41b",
+    english: "caterpillar",
+    mexicanSpanish: "la oruga"
+  },
+  chicken: {
+    emojiCode: "1f413",
+    english: "chicken",
+    mexicanSpanish: "la gallina"
+  },
+  cow: {
+    emojiCode: "1f42e",
+    english: "cow",
+    mexicanSpanish: "la vaca"
+  },
+  crab: {
+    emojiCode: "1f980",
+    english: "crab",
+    mexicanSpanish: "el cangrejo"
+  },
+  crocodile: {
+    emojiCode: "1f40a",
+    english: "crocodile",
+    mexicanSpanish: "el cocodrilo"
+  },
+  deer: {
+    emojiCode: "1f98c",
+    english: "deer",
+    mexicanSpanish: "el venado"
+  },
+  dog: {
+    emojiCode: "1f436",
+    english: "dog",
+    mexicanSpanish: "el perro"
+  },
+  dolphin: {
+    emojiCode: "1f42c",
+    english: "dolphin",
+    mexicanSpanish: "el delfín"
+  },
+  dragon: {
+    emojiCode: "1f409",
+    english: "dragon",
+    mexicanSpanish: "el dragón"
+  },
+  duck: {
+    emojiCode: "1f986",
+    english: "duck",
+    mexicanSpanish: "el pato"
+  },
+  elephant: {
+    emojiCode: "1f418",
+    english: "elephant",
+    mexicanSpanish: "el elefante"
+  },
+  fish: {
+    emojiCode: "1f420",
+    english: "fish",
+    mexicanSpanish: "el pez"
+  },
+  fox: {
+    emojiCode: "1f98a",
+    english: "fox",
+    mexicanSpanish: "el zorro"
+  },
+  frog: {
+    emojiCode: "1f438",
+    english: "frog",
+    mexicanSpanish: "la rana"
+  },
+  goat: {
+    emojiCode: "1f410",
+    english: "goat",
+    mexicanSpanish: "la cabra"
+  },
+  gorilla: {
+    emojiCode: "1f98d",
+    english: "gorilla",
+    mexicanSpanish: "el gorila"
+  },
+  hamster: {
+    emojiCode: "1f439",
+    english: "hamster",
+    mexicanSpanish: "el hamster"
+  },
+  horse: {
+    emojiCode: "1f434",
+    english: "horse",
+    mexicanSpanish: "el caballo"
+  },
+  koala: {
+    emojiCode: "1f428",
+    english: "koala",
+    mexicanSpanish: "el coala"
+  },
+  ladybird: {
+    emojiCode: "1f41e",
+    english: "ladybird",
+    mexicanSpanish: "la catarina"
+  },
+  leopard: {
+    emojiCode: "1f406",
+    english: "leopard",
+    mexicanSpanish: "el leopardo"
+  },
+  lion: {
+    emojiCode: "1f981",
+    english: "lion",
+    mexicanSpanish: "el león"
+  },
+  lizard: {
+    emojiCode: "1f98e",
+    english: "lizard",
+    mexicanSpanish: "la lagartija"
+  },
+  monkey: {
+    emojiCode: "1f435",
+    english: "monkey",
+    mexicanSpanish: "el mono"
+  },
+  mouse: {
+    emojiCode: "1f42d",
+    english: "mouse",
+    mexicanSpanish: "el ratón"
+  },
+  octopus: {
+    emojiCode: "1f419",
+    english: "octopus",
+    mexicanSpanish: "el pulpo"
+  },
+  owl: {
+    emojiCode: "1f989",
+    english: "owl",
+    mexicanSpanish: "el búho"
+  },
+  panda: {
+    emojiCode: "1f43c",
+    english: "panda",
+    mexicanSpanish: "el panda"
+  },
+  penguin: {
+    emojiCode: "1f427",
+    english: "penguin",
+    mexicanSpanish: "el pingüino"
+  },
+  pig: {
+    emojiCode: "1f437",
+    english: "pig",
+    mexicanSpanish: "el cerdo"
+  },
+  rabbit: {
+    emojiCode: "1f430",
+    english: "rabbit",
+    mexicanSpanish: "el conejo"
+  },
+  rat: {
+    emojiCode: "1f400",
+    english: "rat",
+    mexicanSpanish: "la rata"
+  },
+  rhinoceros: {
+    emojiCode: "1f98f",
+    english: "rhinoceros",
+    mexicanSpanish: "el rinoceronte"
+  },
+  scorpion: {
+    emojiCode: "1f982",
+    english: "scorpion",
+    mexicanSpanish: "el escorpión"
+  },
+  shark: {
+    emojiCode: "1f988",
+    english: "shark",
+    mexicanSpanish: "el tiburón"
+  },
+  sheep: {
+    emojiCode: "1f40f",
+    english: "sheep",
+    mexicanSpanish: "el borrego"
+  },
+  shrimp: {
+    emojiCode: "1f990",
+    english: "shrimp",
+    mexicanSpanish: "el camarón"
+  },
+  snail: {
+    emojiCode: "1f40c",
+    english: "snail",
+    mexicanSpanish: "el caracol"
+  },
+  snake: {
+    emojiCode: "1f40d",
+    english: "snake",
+    mexicanSpanish: "la serpiente"
+  },
+  spider: {
+    emojiCode: "1f577",
+    english: "spider",
+    mexicanSpanish: "la araña"
+  },
+  squid: {
+    emojiCode: "1f991",
+    english: "squid",
+    mexicanSpanish: "el calamar"
+  },
+  squirrel: {
+    emojiCode: "1f43f",
+    english: "squirrel",
+    mexicanSpanish: "la ardilla"
+  },
+  turtle: {
+    emojiCode: "1f422",
+    english: "turtle",
+    mexicanSpanish: "la tortuga"
+  },
+  turkey: {
+    emojiCode: "1f983",
+    english: "turkey",
+    mexicanSpanish: "el guajolote"
+  },
+  unicorn: {
+    emojiCode: "1f984",
+    english: "unicorn",
+    mexicanSpanish: "el unicornio"
+  },
+  whale: {
+    emojiCode: "1f433",
+    english: "whale",
+    mexicanSpanish: "la ballena"
+  },
+  wolf: {
+    emojiCode: "1f43a",
+    english: "wolf",
+    mexicanSpanish: "el lobo"
+  }
+};
