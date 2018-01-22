d41caa28e4c18259cd7ef4af99c9407f40135656
Robin Dickson <robindickson@gmail.com>
2018-01-22 21:45:00 +0700

    feat: Add home button to experiment page

--- src/components/Experiment.js
+++ src/components/Experiment.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { updateCurrentExperiment } from "../actions";
 
 class Experiment extends Component {
+  goHome = () => {
+    this.props.dispatch(updateCurrentExperiment("None"));
+  };
+
   render() {
     return (
       <main>
         <h1>{this.props.name}</h1>
+        <button onClick={this.goHome}>Home</button>
       </main>
     );
   }
