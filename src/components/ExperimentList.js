2b270a468271daa7d8147697dc72e9a2c2de0d5f
Robin Dickson <robindickson@gmail.com>
2018-01-22 16:12:57 +0700

    refactor: Move experiment list data to state

--- src/components/ExperimentList.js
+++ src/components/ExperimentList.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 
 class ExperimentList extends Component {
+  state = {
+    experiments: [{ name: "Quiz" }, { name: "Memory Game" }]
+  };
+
   render() {
     return (
       <div>
         <p>Experimental applications for language learning with emoji</p>
-        <ul>
-          <li>Quiz</li>
-          <li>Memory Game</li>
-        </ul>
+        <ul>{this.state.experiments.map(e => <li>{e.name}</li>)}</ul>
       </div>
     );
   }
