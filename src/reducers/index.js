e2ce755fc4eb7895031067f647aa9d47be22cd52
Robin Dickson <robindickson@gmail.com>
2018-01-23 12:09:09 +0700

    feat: Add quiz reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import flashcards from "./flashcards";
 import experiments from "./experiments";
+import quiz from "./quiz";
 
 export default combineReducers({
   experiments,
-  flashcards
+  flashcards,
+  quiz
 });
---
+++ src/reducers/quiz.js
@@ -0,0 +1,13 @@
+const initialQuizState = {
+  questionIndex: 0,
+  score: 0
+};
+
+function quiz(state = initialQuizState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default quiz;
