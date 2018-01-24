99be25ac91bc4c500dc5ae3142e95ef806ed5879
Robin Dickson <robindickson@gmail.com>
2018-01-24 19:17:26 +0700

    feat: Add helper method to create Array of [0..n]

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,3 +3,7 @@ export const shuffle = arr => arr.sort(() => 0.5 - Math.random());
 export const sleep = function(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
+
+export const createArrayOfNumbers = function(n) {
+  return Array.apply(null, { length: n }).map(Function.call, Number);
+};
