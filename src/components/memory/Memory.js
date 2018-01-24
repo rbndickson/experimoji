c55c5b39d4dcc9f6d1edd147e38c06e689d615be
Robin Dickson <robindickson@gmail.com>
2018-01-24 12:46:27 +0700

    feat: Add basic memory game layout

--- src/components/memory/Memory.js
+++ src/components/memory/Memory.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "./memory.css";
 
 class Memory extends Component {
   render() {
     return (
       <main>
-        <section>Match the pictures with the words</section>
+        <p>Match the pictures with the words</p>
+        <div className="memory-flashcards-container">
+          <ul className="memory-flashcards">
+            <li className="memory-flashcard">a</li>
+            <li className="memory-flashcard">b</li>
+            <li className="memory-flashcard">c</li>
+            <li className="memory-flashcard">d</li>
+            <li className="memory-flashcard">a</li>
+            <li className="memory-flashcard">b</li>
+            <li className="memory-flashcard">c</li>
+            <li className="memory-flashcard">d</li>
+          </ul>
+        </div>
       </main>
     );
   }
 }
 
-export default connect()(Memory);
+function mapStateToProps(state) {
+  const flashcards = Object.values(state.flashcards).slice(0, 5);
+  return { flashcards: flashcards };
+}
+
+export default connect(mapStateToProps)(Memory);
