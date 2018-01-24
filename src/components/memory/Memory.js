fb96d78f9df992d05125d70b5bde2590b554f406
Robin Dickson <robindickson@gmail.com>
2018-01-24 15:47:03 +0700

    feat: Add words and images to memory game cards

--- src/components/memory/Memory.js
+++ src/components/memory/Memory.js
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./memory.css";
+import { shuffle } from "../../utils/helpers";
 
 class Memory extends Component {
+  emojiSrc(emojiCode) {
+    return `https://twemoji.maxcdn.com/2/svg/${emojiCode}.svg`;
+  }
+
   render() {
+    let cards = this.props.flashcards.reduce((acc, e) => {
+      acc.push({ english: e.english });
+      acc.push({ emojiCode: e.emojiCode });
+      return acc;
+    }, []);
+
+    cards = shuffle(cards);
+
     return (
       <main>
         <p>Match the pictures with the words</p>
         <div className="memory-flashcards-container">
           <ul className="memory-flashcards">
-            <li className="memory-flashcard">a</li>
-            <li className="memory-flashcard">b</li>
-            <li className="memory-flashcard">c</li>
-            <li className="memory-flashcard">d</li>
-            <li className="memory-flashcard">a</li>
-            <li className="memory-flashcard">b</li>
-            <li className="memory-flashcard">c</li>
-            <li className="memory-flashcard">d</li>
+            {cards.map(
+              flashcard =>
+                flashcard.english ? (
+                  <li key={flashcard.english} className="memory-flashcard">
+                    {flashcard.english}
+                  </li>
+                ) : (
+                  <li key={flashcard.emojiCode} className="memory-flashcard">
+                    <img alt="" src={this.emojiSrc(flashcard.emojiCode)} />
+                  </li>
+                )
+            )}
           </ul>
         </div>
       </main>
@@ -25,7 +42,7 @@ class Memory extends Component {
 }
 
 function mapStateToProps(state) {
-  const flashcards = Object.values(state.flashcards).slice(0, 5);
+  const flashcards = Object.values(state.flashcards).slice(0, 4);
   return { flashcards: flashcards };
 }
 
