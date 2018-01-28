7f373d4ad7f093f66f38ad19c66cff430319776a
Robin Dickson <robindickson@gmail.com>
2018-01-28 16:18:07 +0700

    feat: Add writing practise words

--- src/components/writingWorksheet/WritingWorksheet.js
+++ src/components/writingWorksheet/WritingWorksheet.js
@@ -1,9 +1,39 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./writingWorksheet.css";
 
 class WritingWorksheet extends Component {
   render() {
-    return <div>W O R K S H E E T</div>;
+    return (
+      <div className="writing-practise-container">
+        <h2>Animals</h2>
+        <ul>
+          {this.props.flashcards.map(flashcard => (
+            <li className="writing-practise-words" key={flashcard.english}>
+              <span className="writing-practise-word ">
+                {flashcard.english}
+              </span>
+              <span className="writing-practise-word writing-practise-word-light">
+                {flashcard.english}
+              </span>
+              <span className="writing-practise-word writing-practise-word-light">
+                {flashcard.english}
+              </span>
+              <span className="writing-practise-word writing-practise-word-light">
+                {flashcard.english}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
-export default WritingWorksheet;
+function mapStateToProps(state) {
+  return {
+    flashcards: Object.values(state.flashcards)
+  };
+}
+
+export default connect(mapStateToProps)(WritingWorksheet);
