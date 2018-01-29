f608efb76c7234253913caf14249bd4b6b8f8d8b
Robin Dickson <robindickson@gmail.com>
2018-01-29 16:52:53 +0700

    feat: Adjust experiment title CSS

--- src/components/Experiment.js
+++ src/components/Experiment.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { updateCurrentExperiment } from "../actions";
+import "./Experiment.css";
 import Quiz from "./quiz/Quiz";
 import Memory from "./memory/Memory";
 import WritingWorksheet from "./writingWorksheet/WritingWorksheet";
@@ -13,7 +14,7 @@ class Experiment extends Component {
   render() {
     return (
       <div>
-        <h1>{this.props.name}</h1>
+        <h1 className="Experiment-title">{this.props.name}</h1>
         {this.props.name === "Quiz" && <Quiz />}
         {this.props.name === "Memory Game" && <Memory />}
         {this.props.name === "Writing Practise Worksheet" && (
