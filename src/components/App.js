76b944ac6a36fb618fdedbd9a03d70ad8c5cb37c
Robin Dickson <robindickson@gmail.com>
2018-01-31 11:32:29 +0700

    refactor: Remove unused imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 import AppHeader from "./AppHeader";
 import AppMain from "./AppMain";
-import Menu from "./Menu";
-import Experiment from "./Experiment";
 import "./App.css";
 
 class App extends Component {
