06e8ba50baf9371eafa7e89b3a6dae837a3351f9
Robin Dickson <robindickson@gmail.com>
2018-01-31 18:23:19 +0700

    refactor: Move logic to lifecycle methods

--- src/components/quiz/QuizButtons.js
+++ src/components/quiz/QuizButtons.js
@@ -4,22 +4,48 @@ import { shuffle } from "../../utils/helpers";
 import QuizButton from "./QuizButton";
 
 class QuizButtons extends Component {
+  state = {
+    answers: []
+  };
+
   shouldComponentUpdate(nextProps) {
     return this.props.currentQuestionIndex !== nextProps.currentQuestionIndex;
   }
 
-  render() {
+  componentWillMount() {
+    const answers = this.createAnswers(
+      this.props.flashcards,
+      this.props.flashcard
+    );
+    this.setState({ answers: answers });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const answers = this.createAnswers(
+      nextProps.flashcards,
+      nextProps.flashcard
+    );
+    this.setState({ answers: answers });
+  }
+
+  createAnswers(flashcards, correctFlashcard) {
     const incorrectFlashcards = shuffle(
-      this.props.flashcards.filter(f => f !== this.props.flashcard)
+      flashcards.filter(f => f !== correctFlashcard)
     );
 
-    const answers = shuffle([
-      this.props.flashcard.english,
+    return shuffle([
+      correctFlashcard.english,
       incorrectFlashcards[0].english,
       incorrectFlashcards[1].english
     ]);
+  }
 
-    return <div>{answers.map(e => <QuizButton key={e} answer={e} />)}</div>;
+  render() {
+    return (
+      <div>
+        {this.state.answers.map(e => <QuizButton key={e} answer={e} />)}
+      </div>
+    );
   }
 }
 
