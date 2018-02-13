027ba3ec2cd207213b02b56fe2cb641f72badc47
Robin Dickson <robindickson@gmail.com>
2018-02-13 17:53:08 +0700

    feat: Add image preloader

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { emojiSrc } from "../utils/helpers";
 import Background from "./Background";
 import AppHeader from "./AppHeader";
 import AppMain from "./AppMain";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    this.preloadImages();
+  }
+
+  preloadImages() {
+    this.props.flashcards.forEach(e => {
+      let imageObject = new Image();
+      imageObject.src = emojiSrc(e.emojiCode);
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,7 +31,8 @@ class App extends Component {
 
 function mapStateToProps(state) {
   return {
-    currentExperiment: state.experiments.currentExperiment
+    currentExperiment: state.experiments.currentExperiment,
+    flashcards: Object.values(state.flashcards)
   };
 }
 
