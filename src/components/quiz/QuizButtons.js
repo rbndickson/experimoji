caedf3f1b95a866ed240c9d25fedf0dcfbd88216
Robin Dickson <robindickson@gmail.com>
2018-02-13 14:57:00 +0700

    refactor: Move creation on quiz answers
    To QuizQuestion component to collect game code in one place.

--- src/components/quiz/QuizButtons.js
+++ src/components/quiz/QuizButtons.js
@@ -1,50 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { shuffle } from "../../utils/helpers";
 import "./QuizButtons.css";
 import QuizButton from "./QuizButton";
 
 class QuizButtons extends Component {
-  state = {
-    answers: []
-  };
-
-  shouldComponentUpdate(nextProps) {
-    return this.props.currentQuestionIndex !== nextProps.currentQuestionIndex;
-  }
-
-  componentWillMount() {
-    this.setAnswers(this.props.flashcards, this.props.flashcard);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setAnswers(nextProps.flashcards, nextProps.flashcard);
-  }
-
-  setAnswers(flashcards, flashcard) {
-    const answers = this.createAnswers(Object.values(flashcards), flashcard);
-    this.setState({ answers: answers });
-  }
-
-  createAnswers(flashcards, correctFlashcard) {
-    const incorrectFlashcards = shuffle(
-      flashcards
-        .filter(f => f !== correctFlashcard)
-        .map(flashcard => flashcard.english)
-    );
-
-    const answers =
-      this.props.level === "easy"
-        ? incorrectFlashcards.splice(0, 2)
-        : incorrectFlashcards.splice(0, 5);
-
-    answers.push(correctFlashcard.english);
-
-    return shuffle(answers);
-  }
-
   render() {
-    const { answers } = this.state;
+    const { answers } = this.props;
 
     return this.props.level === "easy" ? (
       <div className={"QuizButtons"}>
@@ -64,12 +25,7 @@ class QuizButtons extends Component {
 }
 
 function mapStateToProps(state) {
-  const flashcards = state.quiz.flashcards;
-
   return {
-    flashcard: flashcards[state.quiz.currentQuestionIndex],
-    flashcards: flashcards,
-    currentQuestionIndex: state.quiz.currentQuestionIndex,
     level: state.quiz.level
   };
 }
--- src/components/quiz/QuizQuestion.js
+++ src/components/quiz/QuizQuestion.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { emojiSrc } from "../../utils/helpers";
+import { emojiSrc, shuffle } from "../../utils/helpers";
 import QuizButtons from "./QuizButtons";
 import "./QuizQuestion.css";
 
 class QuizQuestion extends Component {
+  state = {
+    answers: []
+  };
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.currentQuestionIndex !== nextProps.currentQuestionIndex;
+  }
+
+  componentWillMount() {
+    this.setAnswers(this.props.flashcards, this.props.flashcard);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setAnswers(nextProps.flashcards, nextProps.flashcard);
+  }
+
+  setAnswers(flashcards, flashcard) {
+    const answers = this.createAnswers(Object.values(flashcards), flashcard);
+    this.setState({ answers: answers });
+  }
+
+  createAnswers(flashcards, correctFlashcard) {
+    const incorrectFlashcards = shuffle(
+      flashcards
+        .filter(f => f !== correctFlashcard)
+        .map(flashcard => flashcard.english)
+    );
+
+    const answers =
+      this.props.level === "easy"
+        ? incorrectFlashcards.splice(0, 2)
+        : incorrectFlashcards.splice(0, 5);
+
+    answers.push(correctFlashcard.english);
+
+    return shuffle(answers);
+  }
+
   render() {
     return (
       <div className={"QuizQuestion"}>
@@ -12,7 +50,7 @@ class QuizQuestion extends Component {
           alt={this.props.flashcard.english}
           src={emojiSrc(this.props.flashcard.emojiCode)}
         />
-        <QuizButtons />
+        <QuizButtons answers={this.state.answers} />
       </div>
     );
   }
@@ -22,7 +60,10 @@ function mapStateToProps(state) {
   const flashcards = state.quiz.flashcards;
 
   return {
-    flashcard: flashcards[state.quiz.currentQuestionIndex]
+    flashcard: flashcards[state.quiz.currentQuestionIndex],
+    flashcards: flashcards,
+    currentQuestionIndex: state.quiz.currentQuestionIndex,
+    level: state.quiz.level
   };
 }
 
