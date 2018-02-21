309ec589df06c92a32a98ecd3e052b12473b1519
Robin Dickson <robindickson@gmail.com>
2018-02-21 12:58:10 +0700

    fix: Styling for worksheet printing
    Make width of main element full width.
    
    Commit also includes moving CSS into the JS.

--- src/components/AppMain.js
+++ src/components/AppMain.js
@@ -3,17 +3,43 @@ import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 import Menu from "./Menu";
 import Experiment from "./Experiment";
-import "./AppMain.css";
+import { css, cx } from "emotion";
+
+const AppMainWrapperStyles = css`
+  max-width: 580px;
+  margin: 0 auto;
+`;
+
+const AppMainStyles = css`
+  position: relative;
+  background-color: #fff;
+  max-width: 580px;
+  margin: 0 5px;
+  border-radius: 5px;
+  padding: 10px;
+  z-index: 2;
+`;
+
+const AppMainPrintModifier = css`
+  max-width: none;
+  width: 100%;
+`;
 
 class AppMain extends Component {
   render() {
     return (
-      <div className="AppMain-wrapper">
+      <div
+        className={
+          this.props.currentExperiment === "Writing Practise Worksheet"
+            ? cx(AppMainWrapperStyles, AppMainPrintModifier)
+            : AppMainWrapperStyles
+        }
+      >
         <main
           className={
             this.props.currentExperiment === "Writing Practise Worksheet"
-              ? "AppMain AppMain-print"
-              : "AppMain"
+              ? cx(AppMainStyles, AppMainPrintModifier)
+              : AppMainStyles
           }
         >
           {this.props.currentExperiment === "None" ? (
