e55165f726fd91dbcebcc54e71cdcaeabbbcc9bd
Robin Dickson <robindickson@gmail.com>
2018-02-25 12:54:34 +0700

    feat: Improve start screen layout

--- src/components/quiz/QuizStart.js
+++ src/components/quiz/QuizStart.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { css } from "emotion";
 import { updateQuizScreen } from "../../actions";
 import QuizSettings from "./QuizSettings";
 import Button from "../Button";
+
+const styles = css`
+  * {
+    font-family: "Varela Round", sans-serif;
+  }
+`;
 
 class QuizStart extends Component {
   startQuiz() {
@@ -11,18 +18,20 @@ class QuizStart extends Component {
 
   render() {
     return (
-      <div className="pure-g">
-        <div className="pure-u-1 pure-u-md-2-3">
-          <QuizSettings />
-        </div>
-        <div className="pure-u-1 pure-u-md-1-3">
-          <section>
-            <Button
-              onClick={() => this.startQuiz()}
-              text="Start!"
-              classModifier="Button-green"
-            />
-          </section>
+      <div className={styles}>
+        <div className="pure-g">
+          <div className="pure-u-1 pure-u-md-2-3">
+            <QuizSettings />
+          </div>
+          <div className="pure-u-1 pure-u-md-1-3">
+            <section>
+              <Button
+                onClick={() => this.startQuiz()}
+                text="Start!"
+                classModifier="Button-green"
+              />
+            </section>
+          </div>
         </div>
       </div>
     );
