b33c72b99b2f668b58c6191cbb6094fc4f2d1c54
Robin Dickson <robindickson@gmail.com>
2018-04-24 23:19:16 +0700

    style: Remove unused imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 import BackgroundEmoji from "./BackgroundEmoji";
 import AppHeader from "./AppHeader";
 import AppFooter from "./AppFooter";
