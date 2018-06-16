9345fdc315d41a3d9861ae83002590cf7b922342
Robin Dickson <robindickson@gmail.com>
2018-06-16 23:56:34 +0700

    refactor: Extract creating wordseach data to file

--- src/components/wordsearch/WordSearch.js
+++ src/components/wordsearch/WordSearch.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import update from "immutability-helper";
-import { shuffle } from "../../utils/helpers";
+import { createWordSearch } from "../../utils/wordsearch";
 import Title from "../Title";
 import WordSearchControls from "./WordSearchControls";
 import WordList from "../WordList";
@@ -17,7 +16,7 @@ const initialState = {
 class WordSearch extends Component {
   state = Object.assign(
     initialState,
-    this.createWordSearch(
+    createWordSearch(
       initialState.words,
       initialState.size,
       initialState.isIncludingDiagonals
@@ -40,11 +39,7 @@ class WordSearch extends Component {
 
   updateWords(words) {
     const newState = Object.assign(
-      this.createWordSearch(
-        words,
-        this.state.size,
-        this.state.isIncludingDiagonals
-      ),
+      createWordSearch(words, this.state.size, this.state.isIncludingDiagonals),
       { words }
     );
     this.setState(newState);
@@ -56,11 +51,7 @@ class WordSearch extends Component {
 
   updateSize(size) {
     const newState = Object.assign(
-      this.createWordSearch(
-        this.state.words,
-        size,
-        this.state.isIncludingDiagonals
-      ),
+      createWordSearch(this.state.words, size, this.state.isIncludingDiagonals),
       { size }
     );
     this.setState(newState);
@@ -68,11 +59,7 @@ class WordSearch extends Component {
 
   updateIsIncludingDiagonals(isIncludingDiagonals) {
     const newState = Object.assign(
-      this.createWordSearch(
-        this.state.words,
-        this.state.size,
-        isIncludingDiagonals
-      ),
+      createWordSearch(this.state.words, this.state.size, isIncludingDiagonals),
       { isIncludingDiagonals }
     );
     this.setState(newState);
@@ -84,168 +71,6 @@ class WordSearch extends Component {
 
   clearWordSearch() {
     this.setState({ title: "", words: [], puzzle: {}, answer: {} });
-  }
-
-  createWordSearch(words, size, isIncludingDiagonals) {
-    const placements = shuffle(this.placements(size, isIncludingDiagonals));
-
-    const counts = {
-      east: 0,
-      south: 0,
-      southEast: 0,
-      northEast: 0
-    };
-
-    const answer = words.reduce(
-      (ans, word) => {
-        if (ans) {
-          return this.process(ans, word, placements);
-        } else {
-          return ans;
-        }
-      },
-      { grid: this.createGrid(size), counts: counts }
-    );
-
-    if (answer) {
-      const puzzle = this.fillBlanks(answer.grid);
-      return { puzzle: puzzle, answer: answer.grid, canCreate: true };
-    } else {
-      return { canCreate: false };
-    }
-  }
-
-  createGrid(size) {
-    let grid = {};
-
-    for (var i = 0; i < size; i++) {
-      grid[i] = {};
-      for (var j = 0; j < size; j++) {
-        grid[i][j] = "*";
-      }
-    }
-
-    return grid;
-  }
-
-  placements(size, isIncludingDiagonals) {
-    let directions = isIncludingDiagonals
-      ? ["east", "south", "southEast", "northEast"]
-      : ["east", "south"];
-    return directions.reduce((acc, direction) => {
-      for (var i = 0; i < size; i++) {
-        for (var j = 0; j < size; j++) {
-          acc.push({ row: i, col: j, direction: direction });
-        }
-      }
-      return acc;
-    }, []);
-  }
-
-  process(answer, word, placements) {
-    word = word.replace(/\s+/g, "");
-    const directions = this.directionsSortedByCount(answer.counts);
-
-    // sort placements by preference
-    let sortedPlacements = [];
-
-    directions.forEach(direction => {
-      const p = placements.filter(p => p.direction === direction);
-      sortedPlacements = sortedPlacements.concat(p);
-    });
-
-    for (var i = 0; i < sortedPlacements.length; i++) {
-      const placement = sortedPlacements[i];
-      if (this.canInsert(answer.grid, word, placement)) {
-        const direction = placement.direction;
-        const directionCount = answer.counts[direction];
-
-        return {
-          grid: this.insert(answer.grid, word, placement),
-          counts: { ...answer.counts, [direction]: directionCount + 1 }
-        };
-      }
-    }
-    return false;
-  }
-
-  directionsSortedByCount(directions) {
-    let result = [];
-
-    for (let direction in directions) {
-      result.push([direction, directions[direction]]);
-    }
-
-    return result
-      .sort(function(a, b) {
-        return a[1] - b[1];
-      })
-      .map(e => e[0]);
-  }
-
-  canInsert(grid, word, placement) {
-    let { row, col, direction } = placement;
-    const gridSize = Object.keys(grid).length;
-
-    for (var i = 0; i < word.length; i++) {
-      if (row < 0 || col < 0 || row >= gridSize || col >= gridSize) {
-        return false;
-      } else if (grid[row][col] !== "*" && grid[row][col] !== word[i]) {
-        return false;
-      }
-
-      [row, col] = this.incrementCoordinates(row, col, direction);
-    }
-
-    return true;
-  }
-
-  insert(grid, word, placement) {
-    let { row, col, direction } = placement;
-
-    for (var i = 0; i < word.length; i++) {
-      grid[row][col] = word[i];
-      [row, col] = this.incrementCoordinates(row, col, direction);
-    }
-
-    return grid;
-  }
-
-  incrementCoordinates(row, col, direction) {
-    if (direction === "east") {
-      col += 1;
-    } else if (direction === "south") {
-      row += 1;
-    } else if (direction === "southEast") {
-      col += 1;
-      row += 1;
-    } else if (direction === "northEast") {
-      row -= 1;
-      col += 1;
-    } else {
-      console.log(`Incorrect direction: ${direction}`);
-      return undefined;
-    }
-
-    return [row, col];
-  }
-
-  fillBlanks(grid) {
-    const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
-    const gridSize = Object.keys(grid).length;
-
-    for (var i = 0; i < gridSize; i++) {
-      for (var j = 0; j < gridSize; j++) {
-        if (grid[i][j] === "*") {
-          let randomLetter = ALPHABET[Math.floor(Math.random() * 26)];
-          grid = update(grid, {
-            [i]: { [j]: { $set: randomLetter } }
-          });
-        }
-      }
-    }
-
-    return grid;
   }
 
   updateView(view) {
---
+++ src/utils/wordsearch.js
@@ -0,0 +1,164 @@
+import update from "immutability-helper";
+import { shuffle } from "./helpers";
+
+function placements(size, isIncludingDiagonals) {
+  let directions = isIncludingDiagonals
+    ? ["east", "south", "southEast", "northEast"]
+    : ["east", "south"];
+  return directions.reduce((acc, direction) => {
+    for (var i = 0; i < size; i++) {
+      for (var j = 0; j < size; j++) {
+        acc.push({ row: i, col: j, direction: direction });
+      }
+    }
+    return acc;
+  }, []);
+}
+
+function createGrid(size) {
+  let grid = {};
+
+  for (var i = 0; i < size; i++) {
+    grid[i] = {};
+    for (var j = 0; j < size; j++) {
+      grid[i][j] = "*";
+    }
+  }
+
+  return grid;
+}
+
+function process(answer, word, placements) {
+  word = word.replace(/\s+/g, "");
+  const directions = directionsSortedByCount(answer.counts);
+
+  // sort placements by preference
+  let sortedPlacements = [];
+
+  directions.forEach(direction => {
+    const p = placements.filter(p => p.direction === direction);
+    sortedPlacements = sortedPlacements.concat(p);
+  });
+
+  for (var i = 0; i < sortedPlacements.length; i++) {
+    const placement = sortedPlacements[i];
+    if (canInsert(answer.grid, word, placement)) {
+      const direction = placement.direction;
+      const directionCount = answer.counts[direction];
+
+      return {
+        grid: insert(answer.grid, word, placement),
+        counts: { ...answer.counts, [direction]: directionCount + 1 }
+      };
+    }
+  }
+  return false;
+}
+
+function directionsSortedByCount(directions) {
+  let result = [];
+
+  for (let direction in directions) {
+    result.push([direction, directions[direction]]);
+  }
+
+  return result
+    .sort(function(a, b) {
+      return a[1] - b[1];
+    })
+    .map(e => e[0]);
+}
+
+function canInsert(grid, word, placement) {
+  let { row, col, direction } = placement;
+  const gridSize = Object.keys(grid).length;
+
+  for (var i = 0; i < word.length; i++) {
+    if (row < 0 || col < 0 || row >= gridSize || col >= gridSize) {
+      return false;
+    } else if (grid[row][col] !== "*" && grid[row][col] !== word[i]) {
+      return false;
+    }
+
+    [row, col] = incrementCoordinates(row, col, direction);
+  }
+
+  return true;
+}
+
+function insert(grid, word, placement) {
+  let { row, col, direction } = placement;
+
+  for (var i = 0; i < word.length; i++) {
+    grid[row][col] = word[i];
+    [row, col] = incrementCoordinates(row, col, direction);
+  }
+
+  return grid;
+}
+
+function incrementCoordinates(row, col, direction) {
+  if (direction === "east") {
+    col += 1;
+  } else if (direction === "south") {
+    row += 1;
+  } else if (direction === "southEast") {
+    col += 1;
+    row += 1;
+  } else if (direction === "northEast") {
+    row -= 1;
+    col += 1;
+  } else {
+    console.log(`Incorrect direction: ${direction}`);
+    return undefined;
+  }
+
+  return [row, col];
+}
+
+function fillBlanks(grid) {
+  const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+  const gridSize = Object.keys(grid).length;
+
+  for (var i = 0; i < gridSize; i++) {
+    for (var j = 0; j < gridSize; j++) {
+      if (grid[i][j] === "*") {
+        let randomLetter = ALPHABET[Math.floor(Math.random() * 26)];
+        grid = update(grid, {
+          [i]: { [j]: { $set: randomLetter } }
+        });
+      }
+    }
+  }
+
+  return grid;
+}
+
+export const createWordSearch = (words, size, isIncludingDiagonals) => {
+  const shuffledPlacements = shuffle(placements(size, isIncludingDiagonals));
+
+  const counts = {
+    east: 0,
+    south: 0,
+    southEast: 0,
+    northEast: 0
+  };
+
+  const answer = words.reduce(
+    (ans, word) => {
+      if (ans) {
+        return process(ans, word, shuffledPlacements);
+      } else {
+        return ans;
+      }
+    },
+    { grid: createGrid(size), counts: counts }
+  );
+
+  if (answer) {
+    const puzzle = fillBlanks(answer.grid);
+    return { puzzle: puzzle, answer: answer.grid, canCreate: true };
+  } else {
+    return { canCreate: false };
+  }
+};
